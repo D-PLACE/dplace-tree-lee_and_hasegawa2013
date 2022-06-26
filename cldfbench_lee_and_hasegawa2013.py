ffa8242cc3fd1941b1a7eb4e22e49590dfaed650
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2022-06-26 15:36:50 +1200

    rebuild to fix posterior and data

--- cldfbench_lee_and_hasegawa2013.py
+++ cldfbench_lee_and_hasegawa2013.py
@@ -10,21 +10,22 @@ class Dataset(phlorest.Dataset):
 
     def cmd_makecldf(self, args):
         self.init(args)
-        args.writer.add_summary(
-            self.raw_dir.read_tree('Ainu_SDollo_GRW.mcct.trees', detranslate=True),
-            self.metadata,
-            args.log)
-        
+        # args.writer.add_summary(
+        #     self.raw_dir.read_tree('Ainu_SDollo_GRW.mcct.trees', detranslate=True),
+        #     self.metadata,
+        #     args.log)
+        #
         # set burn-in to 1001, take the rest
-        posterior = self.read_nexus(self.raw_dir / 'Ainu_SDollo_GRW.trees.gz')
-        posterior = self.remove_burnin(posterior, 1001)
-        posterior.trees.detranslate()
-        args.writer.add_posterior(
-            posterior.trees.trees,
-            self.metadata,
-            args.log)
+        posterior = self.raw_dir.read_trees(
+            'Ainu_SDollo_GRW.trees.gz',
+            burnin=1001, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
 
         args.writer.add_data(
             self.raw_dir.read_nexus('Ainu.nex'),
             self.characters,
             args.log)
+            
+
+
+            
